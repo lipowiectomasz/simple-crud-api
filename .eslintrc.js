586e7c3b7940f4e279aeb3a6b0b6fefc4b1fdffd
Tomasz Lipowiec <t.lipowiec@lemonmind.com>
2025-05-13 23:20:26 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended'
+    ],
+    plugins: ['@typescript-eslint'],
+    env: {
+        node: true,
+        es2022: true
+    }
+};
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/server.ts',
+    target: 'node',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
